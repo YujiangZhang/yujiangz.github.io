a46e85921fc5f1c27f9d7f93ac91f394828120e5
张航 <lingyou.ly@outlook.com>
2023-07-15 14:06:02 +0800

    deploy

--- assets/index-f6dbf029.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,i as n,o as t,j as c}from"./index-9b49e745.js";const r={};function s(_,a){const e=n("RouterView");return t(),c(e)}const f=o(r,[["render",s]]);export{f as default};
--- docs/Python/matplotlib.md
+++
@@ -1,9 +0,0 @@
-## 中文问题
-
-```python
-import matplotlib.pyplot as plt
-
-plt.rcParams['font.sans-serif'] = 'SimHei'
-plt.rcParams['axes.unicode_minus'] = False # 用来正常显示负号
-```
-
